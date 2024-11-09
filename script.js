58e08cdaaf9e9110a2dcada11cab9f3f0c5b388a
Mic <126879111+Micjaggs@users.noreply.github.com>
2024-11-09 16:28:06 +1000

    add logic and html

--- script.js
+++ script.js
@@ -1,13 +1,45 @@
 const cnvrtInput = document.getElementById("cnvrt-num")
 const cnvrtBtn = document.getElementById("cnvrt-btn")
-let unitEl = document.getElementsByClassName("cnvrtunit")
+let unitMeter = document.getElementById("cnvrtunit-meter")
+let unitLitre = document.getElementById("cnvrtunit-litre")
+let unitKilo = document.getElementById("cnvrtunit-kilo")
+let unitEl = document.getElementsByClassName("cnvrt-unit")
 
-console.log(unitEl)
+
 
 cnvrtBtn.addEventListener("click", function() {
-    for (let i = 0; i < 4; i ++) {
-        unitEl[i].innerText = cnvrtInput.value
+    for (let i = 0; i < unitEl.length; i ++) {
+       metersFeet()
+       litreGallons()
+       kiloPounds()
     }
 })
 
 
+function metersFeet() {
+    let equation = cnvrtInput.value * 3.28084
+    let equation2 = cnvrtInput.value / 3.28084
+    unitMeter.innerText = `${cnvrtInput.value}  meters = ${Math.round(equation)} feet | ${cnvrtInput.value} feet = ${equation2} meters`
+}
+
+function litreGallons() {
+    let equation = cnvrtInput.value * 0.264172
+    let equation2 = cnvrtInput.value / 0.264172
+    unitLitre.innerText = `${cnvrtInput.value}  litres = ${equation} gallons | ${cnvrtInput.value} gallons = ${equation2} litres`
+}
+
+function kiloPounds() {
+    let equation = cnvrtInput.value * 2.2
+    let equation2 = cnvrtInput.value / 2.2
+    unitKilo.innerText = `${cnvrtInput.value}  kilograms = ${equation} pounds | ${cnvrtInput.value} pounds = ${equation2} kilograms`
+}
+ 
+
+// cnvrtBtn.addEventListener("click", metersFeet, litreGallons, kiloPounds)
+
+
+
+
+
+//multiply the given meter value by 3.28084 feet
+//gallons = 0.264172 × liters
